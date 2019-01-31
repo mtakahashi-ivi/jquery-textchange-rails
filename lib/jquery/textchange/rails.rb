aa2a8161c218cdd2b100023381ddee52f876561a
Toshiyuki HINA <thina@yourinventit.com>
2019-01-31 19:32:14 +0900

    initial commit

---
+++ lib/jquery/textchange/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/textchange/rails/version"
+
+module Jquery
+  module Textchange
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
